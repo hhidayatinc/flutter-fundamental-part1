2abf07513e14aceda2aaef82399baa02d50fa301
hidayatinch <hidayatinc13@gmail.com>
2022-08-29 20:47:57 +0700

    datetimepicker widget

---
+++ lib/basic_widgets/date_time_pickers.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class DateTimePicker extends StatefulWidget{
+  const DateTimePicker({Key? key}) : super(key: key);
+
+  @override
+  DateTimePickerState createState() => DateTimePickerState();
+}
+
+class DateTimePickerState extends State<DateTimePicker>{
+DateTime selectedDate = DateTime.now();
+
+  //  Initial SelectDate FLutter
+  Future<void> _selectDate(BuildContext context) async {
+    // Initial DateTime FIinal Picked
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("${selectedDate.toLocal()}".split(' ')[0]),
+            const SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
+              onPressed: () => {
+                _selectDate(context),
+                // ignore: avoid_print
+                print(selectedDate.day + selectedDate.month + selectedDate.year)
+              },
+              child: const Text('Pilih Tanggal'),
+            ),
+          ],
+        ),
+      );
+  }
+}
